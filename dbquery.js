b632bb55fcd5804ee2d545132db5a475765d5d9d
abhilashak2k <abhilashak2k@gmail.com>
2020-01-03 13:02:49 +0530

    Beautified

--- dbquery.js
+++ dbquery.js
@@ -1,130 +1,57 @@
 const mysql = require('mysql');
 
 const poolDB = mysql.createPool({
-  connectionLimit : 50,
-  host : "localhost",
-  user : "root",
-  password : "",
-  database : "bustracking"
+    connectionLimit: 50,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bustracking"
 });
 
 
-exports.UpdateConductorRouteInfo = (req,res)=>{
+exports.UpdateConductorRouteInfo = (req, res) => {
 
     var busno = req.body.BusNo;
     var shift = req.body.Shift;
 
-    poolDB.getConnection(function (err, conn) {
+    poolDB.getConnection(function(err, conn) {
 
-        conn.query(`SELECT route_id FROM route WHERE bus_id = ${busno} AND shift = "${shift}"`, (err, data)=>{
-          if(err)
-            console.log(err);
-          else{
-            console.log("SQL query result is " + data[0].route_id);
-            res.status(200).send((data[0].route_id).toString());
-          }
+        conn.query(`SELECT route_id
+                    FROM   route
+                    WHERE  bus_id = ${busno}
+                    AND    shift = "${shift}"`, (err, data) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log("SQL query result is " + data[0].route_id);
+                res.status(200).send((data[0].route_id).toString());
+            }
         })
-
     });
-  }
+}
 
 
-exports.UpdateParentRouteInfo = (req, res) =>{
+exports.UpdateParentRouteInfo = (req, res) => {
 
     var parent_no = req.body.PhNo;
     console.log("IN UpdateParentRouteInfo");
 
-    poolDB.getConnection((err, conn)=>{
+    poolDB.getConnection((err, conn) => {
 
-      conn.query(`SELECT route_id FROM student WHERE _id = (SELECT child_id FROM parent_child WHERE parent_id = (SELECT _id FROM parent WHERE phone = "${parent_no}"))`, (err, data)=>{
-        if(err)
-          console.log(err);
-        else{
-            console.log(data);
-            res.status(200).send((data[0].route_id).toString());
-        }
-      })
+        conn.query(`SELECT route_id
+                  FROM   student
+                  WHERE  _id = (SELECT child_id
+                                FROM   parent_child
+                                WHERE  parent_id = (SELECT _id
+                                                    FROM   parent
+                                                    WHERE  phone = "${parent_no}")) `, (err, data) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log(data);
+                res.status(200).send((data[0].route_id).toString());
+            }
+        })
 
     });
-
-
-
 }
-
-
-
-
-
-
-
-// var ParentDetail = require('./models/ParentDetail.model');
-// var BusDetail = require('./models/BusDetail.model');
-// var RouteDetail = require('./models/RouteDetail.model');
-// var ConductorDetail = require('./models/ConductorDetails.model');
-//
-// var mongoose = require('mongoose');
-//
-// exports.UpdateConductorRouteInfo = (req,res)=>{
-//
-//     var busno = req.body.BusNo;
-//     var shift = req.body.Shift;
-//
-//     console.log("in dbquery");
-//
-//     BusDetail.findOne({'BusNo': busno})
-//     .then((businfo)=>{
-//         console.log(businfo);
-//         var routeid = "unable to find";
-//
-//         var arr1 = [...businfo.ShiftArray];
-//         const found = arr1.find(element => element['Shift'] === shift);
-//
-//             if (typeof found !== 'undefined')
-//             routeid = found.RouteId
-//
-//             res.send(routeid);
-//
-//     }).catch((error)=>{
-//            res.send("some error");
-//     });
-//
-//     res.send("bus");
-//
-//
-// }
-//
-// exports.FindParentSendNotification = (req,res)=>{
-//
-//     var regno = req.body.regno;
-//
-//     // ChildDetail.find({'Regno':regno})
-//     // .then((childinfo)=>{
-//
-//     // }).catch((error)=>{
-//
-//     // });
-// }
-//
-//
-//
-// exports.addparent = (req,res)=>{
-//
-//     ParentDetail(req.body).save()
-//     .then((info)=>{
-//         res.send('saved');
-//
-//     }).catch((error)=>{
-//         res.send('some error');
-//     })
-// };
-//
-// exports.addparent = (req,res)=>{
-//
-//     ParentDetail(req.body).save()
-//     .then((info)=>{
-//         res.send('saved');
-//
-//     }).catch((error)=>{
-//         res.send('some error');
-//     })
-// };
--- server.js
+++ server.js
@@ -6,40 +6,36 @@ var http = require('http').Server(app);
 const dbquery = require('./dbquery');
 
 const port = process.env.PORT || 4300;
-
-
 var io = require('socket.io')(3600);
 
+io.on('connection', (socket) => {
 
-   io.on('connection', (socket) =>{
+    socket.on('new-user', (room) => {
+        socket.join(room)
 
-        socket.on('new-user', (room) => {
-          socket.join(room)
+    });
 
-        });
+    socket.on('getloc', (message) => {
+        var room = message.room;
+        io.to(room).emit('loc', message);
+    });
 
-        socket.on('getloc', (message) => {
-          var room = message.room;
-          io.to(room).emit('loc', message);
-        });
+    console.log('a user is connected ')
+});
 
-        console.log('a user is connected ')
-   });
-
-   io.on('disconnection', (socket)=>{
-     console.log("user disconnected " + socket );
-   })
+io.on('disconnection', (socket) => {
+    console.log("user disconnected " + socket);
+})
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 
 app.post('/UpdateConductorRouteInfo', dbquery.UpdateConductorRouteInfo);
 app.post('/UpdateParentRouteInfo', dbquery.UpdateParentRouteInfo);
 
 app.use(express.static(path.join(__dirname, 'public')));
-http.listen(port, ()=>console.log(`App running on ${port}`));
-
-
-/**********DB Query**********/
+http.listen(port, () => console.log(`App running on ${port}`));
