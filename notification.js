f4be46ec2efee4726be05be5a2a148a0d9335aa8
armish24 <arpit.cse24@gmail.com>
2020-01-07 13:07:55 +0530

    smssender done

--- notification.js
+++ notification.js
@@ -1,10 +1,35 @@
-exports.sendsms = (req,res)=>{
+require('dotenv').config();
+const request = require('request');
 
 
-    //sending sms logic
+exports.SendSms = (req,resp)=>{
 
-    res.send('SMS notification send');
-}
+    var mssg = req.body.message;
+    var phonenumber = req.body.tosend;
+    
+      var obj = {
+      apikey:process.env.SMSAPIKEY,  //Put your way2sms api key
+      secret:process.env.SMSSECRET,  // Put your way2sms secret key
+      usetype:"stage",
+      phone: phonenumber,
+      message: mssg,
+      senderid: process.env.EMAIL  //Put your way2sms senderid, mostly its your account email id
+    
+      }
+      request.post({
+        url: 'https://www.way2sms.com/api/v1/sendCampaign',
+        body: obj,
+        json: true
+      },function(err,res,body){
+        if(err)console.log(err);
+        else{
+          console.log(res);
+        resp.send(res);
+        }
+    
+    });
+    
+    }
 
 
 exports.sendemail = (req,res)=>{
